025791530c153c67e3e489026b23cf4fc2f8832d
mecolor123456-dev <mecolor123456@gmail.com>
2025-10-24 13:47:12 -0600

    Update carrito.js

--- js/carrito.js
+++ js/carrito.js
@@ -1,36 +1,28 @@
 let carrito = [];
 
 function agregarAlCarrito(producto) {
-  const p = { ...producto, cantidad: 1, seleccionadas: producto.personalizaciones || [] };
+  const p = {...producto, cantidad:1, seleccionadas: producto.personalizaciones || []};
   carrito.push(p);
   actualizarCarrito();
 }
 
 function actualizarCarrito() {
-  const contenedor = document.getElementById("itemsCarrito");
-  contenedor.innerHTML = carrito.map((p,i) => `
+  const cont = document.getElementById("itemsCarrito");
+  cont.innerHTML = carrito.map((p,i)=>`
     <div>
-      <strong>${p.nombre}</strong> ($${p.precio}) 
+      <strong>${p.nombre}</strong> ($${p.precio})
       <button onclick='quitarDelCarrito(${i})'>X</button>
       <p>Personalizaciones: ${p.seleccionadas.join(', ')}</p>
     </div>
   `).join("");
 
-  const total = carrito.reduce((acc,p) => acc + p.precio, 0);
+  const total = carrito.reduce((acc,p)=> acc+p.precio,0);
   document.getElementById("total").innerText = total;
 }
 
-function quitarDelCarrito(index) {
-  carrito.splice(index,1);
-  actualizarCarrito();
-}
+function quitarDelCarrito(i){ carrito.splice(i,1); actualizarCarrito(); }
+function vaciarCarrito(){ carrito=[]; actualizarCarrito(); }
 
-function vaciarCarrito() {
-  carrito = [];
-  actualizarCarrito();
-}
-
-// Hacer funciones accesibles desde HTML
 window.agregarAlCarrito = agregarAlCarrito;
 window.quitarDelCarrito = quitarDelCarrito;
 window.vaciarCarrito = vaciarCarrito;
